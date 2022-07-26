85c32450c837bad98f34e5753bb1041a373da529
Himanshu Khare <himanshush443@gmail.com>
2022-07-26 18:50:58 +0530

    Layout, Header, Footer

---
+++ pages/event/index.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default function EventPage() {
+  return (
+    <Layout title="All events" >
+        <h1>My all events</h1>
+    </Layout>
+  )
+}
